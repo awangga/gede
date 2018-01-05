5199ac97f6445ae8ab9a315c66a55fcb53a3d5cc
Julham Ramadhana <julhamramadhana7@gmail.com>
2018-01-05 23:53:29 +0700

    Update gede.py

--- gede.py
+++ gede.py
@@ -12,6 +12,16 @@ class Gede(object):
 				if a[1] == Universitas Nurtanio:
 						return i
 				i=i+1
+				
+	def __init__(self,Medan):
+		self.sf = shapefile.Reader(Medan)
+		
+	def selectTempat(self, Masjid Raya):
+		i =0
+		for a in self.sf.records():
+				if a[1] == Masjid Raya:
+						return i
+				i=i+1
 
 
 
