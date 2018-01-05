db315949319b6e87498a6e6fe3db0eb5163c8410
akbarpambudiutomo <akbarpambudi31@gmail.com>
2018-01-05 23:49:37 +0700

    first commit

--- gede.py
+++ gede.py
@@ -3,18 +3,15 @@
 import shapefile
 
 class Gede(object):
-	def __init__(self,namafile):
-		self.sf = shapefile.Reader(namafile)
-
-	def itungBaris(self):
-		rec = self.sf.shapes()
-		return len(rec)
-	def selectNegara(self,NEGARA):
-		i = 0
+	def __init__(self,bandung):
+		self.sf = shapefile.Reader(bandung)
+		
+	def selectTempat(self,Universitas Nurtanio):
+		i =0 
 		for a in self.sf.records():
-			if a[8] == NEGARA:
-				return i
-			i=i+1
+				if a[1] == Universitas Nurtanio:
+						return i
+				i=i+1
 
 
 
