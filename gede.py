aff1d63cd159ae593fd65d32a87d7e6ade6afe7f
Kezia Tirza Naramessakh <kezianaramessakh@gmail.com>
2018-01-06 16:48:58 +0000

    Update gede.py

--- gede.py
+++ gede.py
@@ -22,9 +22,11 @@ class Gede(object):
 	def selectJalan(self,Jalan Tubagus Ismail Raya):
 		i = 0
 		for a in self.sf.records():
-			if a[1] == Jalan Tubagus Ismail Raya:
+			if a[1] = Jalan Tubagus Ismail Raya:
 				return i 
 			i = i + 1
+			
+
 			
 
 		
