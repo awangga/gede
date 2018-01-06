3f84950a36fce481904ec6bc7153db7beb320c08
Mohammad Arya Rosyd Sikumbang <arya.mohammad00@gmail.com>
2018-01-06 21:29:54 +0700

    Update gede.py

--- gede.py
+++ gede.py
@@ -5,6 +5,12 @@ import shapefile
 class Gede(object):
 	def __init__(self,namafile):
 		self.sf = shapefile.Reader(namafile)
+		self.parent = master
+        	self.parent.title("LabelTool")
+       	 	self.frame = Frame(self.parent)
+        	self.frame.pack(fill=BOTH, expand=1)
+		
+self.parent.resizable(width = FALSE, height = FALSE)
 
 	def itungBaris(self):
 		rec = self.sf.shapes()
