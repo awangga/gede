af3e400a7ffa3d674b4c681d3c3a29ad3a773284
Kezia Tirza Naramessakh <kezianaramessakh@gmail.com>
2018-01-06 16:49:30 +0000

    Update gede.py

--- gede.py
+++ gede.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+=#!/usr/bin/python
 
 import shapefile
 
@@ -22,7 +22,7 @@ class Gede(object):
 	def selectJalan(self,Jalan Tubagus Ismail Raya):
 		i = 0
 		for a in self.sf.records():
-			if a[1] = Jalan Tubagus Ismail Raya:
+			if a[1] == Jalan Tubagus Ismail Raya:
 				return i 
 			i = i + 1
 			
